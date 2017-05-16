130adacbda39fef77cd55d47396c8f9a73170218
jamesjacobk <jamesjacobtech@gmail.com>
2017-05-17 03:34:17 +0530

    Add Bubble Sort

---
+++ Assets/Scripts/AlgorithmObjectManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlgorithmObjectManager : MonoBehaviour {
+
+    GameObject AlgorithmObject;
+    const int count = 10;
+    const int positionScaler = 10;
+
+    public List<Transform> listOfObjects = new List<Transform>();
+
+    void Awake () {
+        GenerateObjects();
+        RandomizeObjects();
+    }
+
+    void GenerateObjects()
+    {
+        for (int i = 0; i < count; ++i)
+        {
+            listOfObjects.Add(Instantiate(Resources.Load("AlgorithmObject") as GameObject, transform.position + new Vector3(i * positionScaler, 0, 0), Quaternion.identity, transform).transform);
+            listOfObjects[i].gameObject.name = i.ToString();
+        }
+    }
+
+    public void RandomizeObjects()
+    {
+        for (int i = 0; i < count; ++i)
+        {
+            //listOfObjects[i].localScale = Vector3.one + (Vector3.one * Random.value);
+            listOfObjects[i].localScale = Vector3.one * Random.value * 10f;
+        }
+    }
+}
---
+++ Assets/Scripts/BubbleSort.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSort : MonoBehaviour
+{
+    List<Transform> listOfObjects;
+
+    void Awake()
+    {
+        listOfObjects = gameObject.GetComponent<AlgorithmObjectManager>().listOfObjects;
+    }
+
+	public void Sort()
+    {
+        for (int i = listOfObjects.Count - 1; i >= -1; --i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (listOfObjects[j].localScale.x > listOfObjects[j + 1].localScale.x)
+                {
+                    Debug.Log("Swapping...");
+                    swapInList(j);
+                    swapVisually(j);
+                }
+            }
+        }
+    }
+
+    void swapInList(int index)
+    {
+        var temp = listOfObjects[index + 1];
+        listOfObjects[index + 1] = listOfObjects[index];
+        listOfObjects[index] = temp;
+    }
+
+    void swapVisually(int index)
+    {
+        var temp = listOfObjects[index + 1].position;
+        listOfObjects[index + 1].position = listOfObjects[index].position;
+        listOfObjects[index].position = temp;
+    }
+}
